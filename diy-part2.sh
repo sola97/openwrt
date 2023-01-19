b22b7d9fe374e7b6f5515a43bfd726d72ee4ea80
sola97 <sola97@qq.com>
2023-01-19 07:00:54 +0000

    update nuc

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,7 +15,7 @@ HOSTNAME=K2P-Nuc
 IPADDRESS=192.168.2.1
 SSID=k2p
 ENCRYPTION=psk2+ccmp
-PASSWORD=24094572
+KEY=24094572
 ###############################################################
 
 #修改路由器名称
@@ -23,15 +23,6 @@ sed -i "s/hostname='OpenWrt'/hostname='$HOSTNAME'/g" package/base-files/files/bi
 
 #修改默认IP
 sed -i 's/192.168.1.1/$IPADDRESS/g' package/base-files/files/bin/config_generate
-
-# 修改默认wifi名称ssid为$SSID
-sed -i 's/ssid=OpenWrt/ssid='$SSID'/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-
-# 修改默认wifi密码key为password
-sed -i 's/encryption=none/encryption='$ENCRYPTION'/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-
-#使用sed 在第四行后添加新字
-sed -i '/set wireless.default_radio${devidx}.encryption='$ENCRYPTION'/a\set wireless.default_radio${devidx}.key='$PASSWORD'' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 # 修改版本号
 sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(TZ=UTC-8 date +%Y.%m.%d)'|g" package/lean/default-settings/files/zzz-default-settings
@@ -49,3 +40,15 @@ sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/theme
 # 修改 netgear 为默认主题
 sed -i 's/luci-theme-bootstrap/luci-theme-netgear/g' feeds/luci/collections/luci/Makefile
 
+
+# Modify default WiFi SSID
+sed -i "s/set wireless.default_radio\${devidx}.ssid=OpenWrt/set wireless.default_radio\${devidx}.ssid='$SSID'/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# Modify default WiFi Encryption
+sed -i "s/set wireless.default_radio\${devidx}.encryption=none/set wireless.default_radio\${devidx}.encryption='$ENCRYPTION'/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# Modify default WiFi Key
+sed -i "/set wireless.default_radio\${devidx}.mode=ap/a\                        set wireless.default_radio\${devidx}.key='$KEY'" package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# Forced WiFi to enable
+sed -i 's/set wireless.radio\${devidx}.disabled=1/set wireless.radio\${devidx}.disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
